33c7d88594b0e7e8e39a6746621121a1a444da7a
Marvin Stickel <marv.stickel@gmail.com>
2023-08-14 16:29:44 +0200

    Update: Show Images, add Spinner and ErrorAlert

--- src/api/jsonPlaceholder.ts
+++ src/api/jsonPlaceholder.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ImageLoaderProps } from "next/image";
 
 const baseURL = "https://jsonplaceholder.typicode.com";
 
@@ -6,4 +7,12 @@ export const ALBUMS_QUERY = ["albums"];
 export const getAlbums = async () => {
   const url = `${baseURL}/albums/1/photos`;
   return await axios.get<JSONPlaceholder.Album[]>(url).then((res) => res.data);
+};
+
+export const jsonPlaceholderImageLoader = ({
+  src,
+  width,
+  quality,
+}: ImageLoaderProps) => {
+  return src;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,51 @@
 "use client";
 
-import { ALBUMS_QUERY, getAlbums } from "@/api/jsonPlaceholder";
+import {
+  ALBUMS_QUERY,
+  getAlbums,
+  jsonPlaceholderImageLoader,
+} from "@/api/jsonPlaceholder";
+import ErrorAlert from "@/components/ErrorAlert";
+import Spinner from "@/components/Spinner";
 import { useQuery } from "@tanstack/react-query";
 import { isAxiosError } from "axios";
+import Image from "next/image";
 
 export default function Home() {
   const { isLoading, error, data } = useQuery({
     queryKey: ALBUMS_QUERY,
     queryFn: getAlbums,
   });
-  console.log("Home -> isLoading, error, data:", isLoading, error, data);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
 
   if (error && isAxiosError<JSONPlaceholder.Album[]>(error))
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <ErrorAlert message={error.message} />
+      </div>
+    );
 
-  if (data) return <div>Albums: {data.length}</div>;
+  if (data)
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        {data.map((album) => (
+          <Image
+            key={album.id}
+            loader={jsonPlaceholderImageLoader}
+            src={album.url}
+            alt={album.title}
+            width={500}
+            height={500}
+          />
+        ))}
+      </div>
+    );
 
   return null;
 }
